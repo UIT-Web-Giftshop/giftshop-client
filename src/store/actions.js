299a9562369ec11e35c465dcd58eb3235eb5ded4
Minhthang1506 <19522216@gm.uit.edu.vn>
2022-04-15 14:53:30 +0700

    productpage

---
+++ src/store/actions.js
@@ -0,0 +1,5 @@
+export default {
+    someFunction: function({commit}) {
+        commit('setProduct',null)
+    },
+}
---
+++ src/store/mutations.js
@@ -0,0 +1,5 @@
+export default {
+    setProduct: function(state, product) {
+        state.product = product
+    } 
+}
