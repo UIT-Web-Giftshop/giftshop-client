682c5f44ae202997f3e5d8f361290a9414031905
Phạm Tấn khang <phamtankhang2001@gmail.com>
2022-06-11 15:36:20 +0700

    handle router authorize + 401Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import routes from './router'
 import VueRouter from 'vue-router'
+import store from '../store'
 
 Vue.use(VueRouter)
 
@@ -8,5 +9,22 @@ const router = new VueRouter({
   mode: 'history',
   routes
 })
+
+
+/**
+ * redirect to login page when authenticated fail
+*/
+router.beforeEach((to, from, next) => {
+  const authenticated = store.getters['auth/isAuthendicated'];
+
+  //check user authendicated, return Login page if it not available
+  if(!authenticated && to.meta.requireAuth ){
+    next('/401');
+    return;
+  }
+
+  next()
+})
+
  
 export default router
