438c677913f2d23e2e7311a3c52fe5008f8d21f8
ds <19520216@gm.uit.edu.vn>
2022-06-11 12:14:24 +0700

    update paging and loading progress

---
+++ src/components/list-product/VProgress.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-progress-linear
+    :active="(this.getItemsCount == 0) ? true : false"
+    :indeterminate="(this.getItemsCount == 0) ? true : false"
+    fixed
+    top
+    height="2"
+    color="red"
+  ></v-progress-linear>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  name: "VProgress",
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    loading: () => {
+      return true;
+    },
+    ...mapGetters({
+      getItemsCount: 'list_products/getItemsCount'
+    })
+  }
+};
+</script>
